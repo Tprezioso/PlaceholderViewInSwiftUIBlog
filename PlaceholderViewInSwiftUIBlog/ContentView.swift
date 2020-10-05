70b999cb9a12e3358105721ec3ae3a34641c57de
Tprezioso <tommyprezioso@gmail.com>
2020-10-05 08:56:39 -0400

    Initial Commit

---
+++ PlaceholderViewInSwiftUIBlog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlaceholderViewInSwiftUIBlog
+//
+//  Created by Thomas Prezioso on 10/5/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlaceholderViewInSwiftUIBlog/PlaceholderViewInSwiftUIBlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceholderViewInSwiftUIBlogApp.swift
+//  PlaceholderViewInSwiftUIBlog
+//
+//  Created by Thomas Prezioso on 10/5/20.
+//
+
+import SwiftUI
+
+@main
+struct PlaceholderViewInSwiftUIBlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
